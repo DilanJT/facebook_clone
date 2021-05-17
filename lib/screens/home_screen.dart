751574e6d6929f699fbe5f7a2150407450ac9db3
DilanJT <dicmandilan@gmail.com>
2021-05-17 14:28:56 +0530

    starter code initialized

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              "Facebook"
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
