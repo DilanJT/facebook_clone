fa752e3fa0d95735b53eecec8594172da2cb7b9e
DilanJT <dicmandilan@gmail.com>
2021-05-18 01:42:05 +0530

    facebook bottom app bar functionality done

---
+++ lib/screens/nav_screens.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/screens/screens.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class NavScreen extends StatefulWidget {
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  final List<Widget> _screens = [
+    HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+  final List<IconData> _icons = const [
+    Icons.home,
+    Icons.ondemand_video,
+    MdiIcons.accountCircleOutline,
+    MdiIcons.accountGroupOutline,
+    MdiIcons.bellOutline,
+    Icons.menu,
+  ];
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _icons.length,
+      child: Scaffold(
+        // in indexed stack when we change the tabs we maintain our scroll position
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _screens,
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(bottom: 0.0),
+          child: CustomTabBar(
+            icons: _icons,
+            selectedIndex: _selectedIndex,
+            onTap: (index) => setState(() => _selectedIndex = index),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
